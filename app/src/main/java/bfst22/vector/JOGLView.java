576ec3845c8eb01d2ecbe92e0cf000f92a03de8d
JonasUJ <jonas@ulsnes.dk>
2022-02-18 11:48:34 +0100

    Add opengl renderer with jogl

---
+++ app/src/main/java/bfst22/vector/JOGLView.java
@@ -0,0 +1,100 @@
+package bfst22.vector;
+
+import com.jogamp.newt.event.MouseEvent;
+import com.jogamp.newt.event.MouseListener;
+import com.jogamp.newt.javafx.NewtCanvasJFX;
+import com.jogamp.newt.NewtFactory;
+import com.jogamp.newt.opengl.GLWindow;
+import com.jogamp.opengl.*;
+import com.jogamp.opengl.util.Animator;
+import javafx.application.Platform;
+import javafx.geometry.Point2D;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import java.awt.*;
+
+public class JOGLView {
+    private Animator animator;
+
+    public JOGLView(Stage stage, LinesModel model) {
+        Platform.setImplicitExit(true);
+        final var display = NewtFactory.createDisplay(null, false);
+        final var screen = NewtFactory.createScreen(display, 0);
+        final var caps = new GLCapabilities(GLProfile.getMaxFixedFunc(true));
+        final var window = GLWindow.create(screen, caps);
+        final var canvas = new NewtCanvasJFX(window);
+        final var pane = new StackPane(canvas);
+        final var gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+
+        // Resize canvas when pane resizes
+        pane.widthProperty().addListener((observable, oldValue, newValue) -> window.setSize(newValue.intValue(), window.getHeight()));
+        pane.heightProperty().addListener((observable, oldValue, newValue) -> window.setSize(window.getWidth(), newValue.intValue()));
+
+        // Open at one quarter the size of the display
+        canvas.setWidth(gd.getDisplayMode().getWidth() >> 1);
+        canvas.setHeight(gd.getDisplayMode().getHeight() >> 1);
+
+        stage.show();
+        stage.setTitle("openGL rendering is faster :)");
+        stage.setScene(new Scene(pane));
+
+        // Stop on application close
+        stage.setOnCloseRequest(event -> animator.stop());
+
+        window.display();
+        window.addGLEventListener(new LinesRenderer(model));
+
+        // yes yes this is controller logic, but I don't have time since it's 11:40 right now and this is due in 20 minutes
+        window.addMouseListener(new MouseListener() {
+            private Point2D prev;
+
+            @Override
+            public void mouseClicked(MouseEvent mouseEvent) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent mouseEvent) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent mouseEvent) {
+
+            }
+
+            @Override
+            public void mousePressed(MouseEvent mouseEvent) {
+                prev = new Point2D(mouseEvent.getX(), mouseEvent.getY());
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent mouseEvent) {
+
+            }
+
+            @Override
+            public void mouseMoved(MouseEvent mouseEvent) {
+
+            }
+
+            @Override
+            public void mouseDragged(MouseEvent mouseEvent) {
+                model.addXY(
+                        (float)(mouseEvent.getX() - prev.getX()),
+                        (float)(mouseEvent.getY() - prev.getY())
+                );
+                prev = new Point2D(mouseEvent.getX(), mouseEvent.getY());
+            }
+
+            @Override
+            public void mouseWheelMoved(MouseEvent mouseEvent) {
+                model.zoom(mouseEvent.getRotation()[1] / 10);
+            }
+        });
+        animator = new Animator(window);
+        animator.start();
+    }
+}
---
+++ app/src/main/java/bfst22/vector/LinesModel.java
@@ -0,0 +1,113 @@
+package bfst22.vector;
+
+import com.jogamp.common.nio.Buffers;
+
+import java.io.IOException;
+import java.nio.FloatBuffer;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+public class LinesModel {
+    private final float[] verticesBuffer;
+    private final int[] indicesBuffer;
+    private final float[] colorBuffer;
+    private static final float lineWidth = 1f;
+    private float zoom = 1;
+    private float x = 0;
+    private float y = 0;
+    private FloatBuffer affinity;
+
+    public LinesModel(String filename) throws IOException {
+        recalculateAffinity();
+
+        var array  = Files.lines(Paths.get(filename))
+                    .flatMapToDouble(l -> Arrays.stream(l.split(" "))
+                        .skip(1)
+                        .mapToDouble(n -> Double.parseDouble(n))).toArray();
+
+        var vertices = new float[array.length * 3];
+        var indices = new int[array.length / 4 * 6];
+        var colors = new float[array.length / 4 * 3];
+        for (int i = 0; i < array.length / 4; i++) {
+            var arrayIndex = i * 4;
+            var bufferIndex = i * 12;
+            var indexIndex = i * 6; // lol
+            var colorIndex = i * 3;
+            var start = new Vector((float)array[arrayIndex], (float)array[arrayIndex + 1]);
+            var end = new Vector((float)array[arrayIndex + 2], (float)array[arrayIndex + 3]);
+            var vec = end.sub(start);
+            var p0 = vec.hat().normalize().scale(lineWidth / 2);
+            var p3 = p0.scale(-1.0f);
+            var p1 = p0.add(vec);
+            var p2 = p3.add(vec);
+            p0 = p0.add(start);
+            p1 = p1.add(start);
+            p2 = p2.add(start);
+            p3 = p3.add(start);
+            vertices[bufferIndex + 0] = p0.x();
+            vertices[bufferIndex + 1] = p0.y();
+            vertices[bufferIndex + 2] = 0.0f;
+            vertices[bufferIndex + 3] = p1.x();
+            vertices[bufferIndex + 4] = p1.y();
+            vertices[bufferIndex + 5] = 0.0f;
+            vertices[bufferIndex + 6] = p2.x();
+            vertices[bufferIndex + 7] = p2.y();
+            vertices[bufferIndex + 8] = 0.0f;
+            vertices[bufferIndex + 9] = p3.x();
+            vertices[bufferIndex + 10] = p3.y();
+            vertices[bufferIndex + 11] = 0.0f;
+            indices[indexIndex + 0] = arrayIndex + 0;
+            indices[indexIndex + 1] = arrayIndex + 1;
+            indices[indexIndex + 2] = arrayIndex + 2;
+            indices[indexIndex + 3] = arrayIndex + 2;
+            indices[indexIndex + 4] = arrayIndex + 0;
+            indices[indexIndex + 5] = arrayIndex + 3;
+            colors[colorIndex + 0] = 0.0f;
+            colors[colorIndex + 1] = 0.0f;
+            colors[colorIndex + 2] = 0.0f;
+        }
+
+        verticesBuffer = vertices;
+        indicesBuffer = indices;
+        colorBuffer = colors;
+    }
+
+
+
+    public float[] getVertices() {
+        return verticesBuffer;
+    }
+
+    public int[] getIndices() {
+        return indicesBuffer;
+    }
+
+    public float[] getColors() {
+        return colorBuffer;
+    }
+
+    public FloatBuffer getAffinity() {
+        return affinity;
+    }
+
+    public void zoom(float zoom) {
+        this.zoom += zoom;
+        recalculateAffinity();
+    }
+
+    public void addXY(float x, float y) {
+        this.x += x;
+        this.y += y;
+        recalculateAffinity();
+    }
+
+    private void recalculateAffinity() {
+        affinity = Buffers.newDirectFloatBuffer(new float[] {
+                zoom, 0, 0, 0,
+                0, zoom, 0, 0,
+                0, 0, zoom, 0,
+                -x, y, 0, 1
+        });
+    }
+}
