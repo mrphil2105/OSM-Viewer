17fd8da539d4332faba1ea44f16e8a9d76e7eeac
magnusblarsen <magnusbrunlarsen00@gmail.com>
2022-03-07 10:33:15 +0100

    Refactor fileparser and add regex

--- app/src/main/java/canvas/FileParser.java
+++ app/src/main/java/canvas/FileParser.java
@@ -12,63 +12,62 @@ import java.util.zip.ZipOutputStream;
 public class FileParser {
 
     public static Polygons readFile(String filename) throws IOException, XMLStreamException {
-        // TODO: Clean up file reading/writing. Also, I was using Serializable before it
-        // was cool...
-
 
         Polygons polygons;
 
-        if (filename.endsWith(".osm")
-                || filename.endsWith(".xml")
-                || filename.endsWith(".osm.zip")
-                || filename.endsWith(".xml.zip")) {
+        if (filename.matches(".*(\\.osm|\\.xml)(\\.zip)?$")) {
 
-            InputStream stream = new BufferedInputStream(new FileInputStream(filename));
-            if (filename.endsWith(".zip")) {
-                var zipFile = new ZipFile(filename);
-                var entry = zipFile.entries().nextElement();
-                stream = zipFile.getInputStream(entry);
-            }
-
-            var reader = new OSMReader(stream);
+            var reader = new OSMReader(getInputStream(filename));
             polygons = reader.createPolygons();
             filename = filename.split("\\.")[0] + ".ser.zip";
 
             serializeObject(polygons, filename);
-
         } else if (filename.endsWith(".ser") || filename.endsWith(".ser.zip")) {
-            try {
-                InputStream stream;
-                if (filename.endsWith(".zip")) {
-                    var zipFile = new ZipFile(filename);
-                    var entry = zipFile.entries().nextElement();
-                    stream = zipFile.getInputStream(entry);
-                } else {
-                    stream = new BufferedInputStream(new FileInputStream(filename));
-                }
-
-                stream = new ObjectInputStream(stream);
-                polygons = (Polygons) ((ObjectInputStream) stream).readObject();
-                stream.close();
-            } catch (IOException | ClassNotFoundException e) {
-                throw new RuntimeException("Could not read serialized file: " + e.getMessage());
-            }
+            polygons = deserializeToPolygons(filename);
         } else {
+            System.out.println("LSDKFJSDLKFJLSDKFJ");
+            System.out.println(filename);
             throw new IllegalArgumentException(
                     "Only .osm, .xml, .ser or any of those zipped are allowed");
         }
-        return null;
+        return polygons;
     }
 
-    static void serializeObject(Serializable serializable, String filename) throws IOException{
-        //TODO: where does file get created???
+    static InputStream getInputStream(String filename) throws IOException{
 
-        var zipStream = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(filename)));
-        zipStream.putNextEntry(new ZipEntry(filename));
-        var outputStream = new ObjectOutputStream(zipStream);
-        outputStream.writeObject(serializable);
-        outputStream.flush();
-        outputStream.close();
+        if (filename.endsWith(".zip")) {
+            var zipFile = new ZipFile(filename);
+            var entry = zipFile.entries().nextElement();
+            return zipFile.getInputStream(entry);
+        }
+
+        return new BufferedInputStream(new FileInputStream(filename));
+    }
+
+    static void serializeObject(Serializable serializable, String filename) {
+
+        try(var zipStream = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(filename)));
+            var outputStream = new ObjectOutputStream(zipStream);
+        ){
+            zipStream.putNextEntry(new ZipEntry(filename));
+            outputStream.writeObject(serializable);
+
+        } catch(IOException e){
+            throw new RuntimeException("Could not serialize object: " + e.getMessage());
+        }
+
+    }
+
+    static Polygons deserializeToPolygons(String filename){
+        try (InputStream inputStream = getInputStream(filename);
+             ObjectInputStream objectInputStream = new ObjectInputStream(inputStream)
+        ){
+
+            return (Polygons) objectInputStream.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException("Could not read serialized file: " + e.getMessage());
+        }
+
     }
 }
 
