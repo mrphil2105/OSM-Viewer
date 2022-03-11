09bc77e99882ea7135085a84d4c32c4713f71417
Philip Mørch <mrphil2105@gmail.com>
2022-03-11 16:19:26 +0100

    Serialize and deserialize multiple objects in one ZIP file

--- app/src/main/java/canvas/FileParser.java
+++ app/src/main/java/canvas/FileParser.java
@@ -2,6 +2,9 @@ package canvas;
 
 import drawing.Polygons;
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 import java.util.zip.ZipOutputStream;
@@ -9,6 +12,7 @@ import javax.xml.stream.XMLStreamException;
 import navigation.Dijkstra;
 import osm.OSMReader;
 
+// TODO: Use a custom exception type instead of RuntimeException for when parsing fails.
 public class FileParser {
 
     public static ReadResult readFile(String filename) throws IOException, XMLStreamException {
@@ -23,12 +27,13 @@ public class FileParser {
             reader.addObserver(dijkstra);
             reader.parse(getInputStream(filename));
 
-            filename = filename.split("\\.")[0] + ".ser.zip";
-            serializeObject(polygons, filename);
-        } else if (filename.endsWith(".ser") || filename.endsWith(".ser.zip")) {
-            polygons = deserializeToPolygons(filename);
+            filename = filename.split("\\.")[0] + ".zip";
+            serializeObjects(filename, polygons, dijkstra);
+        } else if (filename.endsWith(".zip")) {
+            var objects = deserializeObjects(filename, Polygons.class, Dijkstra.class);
+            polygons = (Polygons) objects.get(0);
+            dijkstra = (Dijkstra) objects.get(1);
         } else {
-            System.out.println("LSDKFJSDLKFJLSDKFJ");
             System.out.println(filename);
             throw new IllegalArgumentException(
                     "Only .osm, .xml, .ser or any of those zipped are allowed");
@@ -38,7 +43,6 @@ public class FileParser {
     }
 
     private static InputStream getInputStream(String filename) throws IOException {
-
         if (filename.endsWith(".zip")) {
             var zipFile = new ZipFile(filename);
             var entry = zipFile.entries().nextElement();
@@ -48,31 +52,48 @@ public class FileParser {
         return new BufferedInputStream(new FileInputStream(filename));
     }
 
-    private static void serializeObject(Serializable serializable, String filename) {
-
+    private static void serializeObjects(String filename, Serializable... serializableArr) {
         try (var zipStream =
-                new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(filename))); ) {
-            zipStream.putNextEntry(new ZipEntry(filename));
+                new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(filename)))) {
+            for (var serializable : serializableArr) {
+                var className = serializable.getClass().getSimpleName();
 
-            try (var outputStream = new ObjectOutputStream(zipStream)) {
-                outputStream.writeObject(serializable);
-            } catch (IOException e) {
-                throw new RuntimeException(
-                        "Could not serialize object: " + e.getMessage()); // TODO: mere præcis besked
+                zipStream.putNextEntry(new ZipEntry(className + ".ser"));
+
+                // Do not close the ObjectOutputStream as it closes the ZipOutputStream.
+                var objectStream = new ObjectOutputStream(zipStream);
+                objectStream.writeObject(serializable);
             }
-
         } catch (IOException e) {
-            throw new RuntimeException("Could not serialize object: " + e.getMessage()); // TODO
+            throw new RuntimeException("Could not serialize object: " + e.getMessage());
         }
     }
 
-    private static Polygons deserializeToPolygons(String filename) {
-        try (InputStream inputStream = getInputStream(filename);
-                ObjectInputStream objectInputStream = new ObjectInputStream(inputStream)) {
+    @SuppressWarnings("rawtypes")
+    private static List<Serializable> deserializeObjects(String filename, Class... classes) {
+        var objects = new ArrayList<Serializable>();
 
-            return (Polygons) objectInputStream.readObject();
+        try (var zipFile = new ZipFile(filename)) {
+            var classNames = Arrays.stream(classes).map(Class::getSimpleName).toList();
+
+            for (var className : classNames) {
+                var entryName = className + ".ser";
+                var entry = zipFile.getEntry(entryName);
+
+                if (entry == null) {
+                    throw new RuntimeException("Zip entry with name '" + entryName + "' is not present.");
+                }
+
+                var inputStream = zipFile.getInputStream(entry);
+                var objectStream = new ObjectInputStream(inputStream);
+                var object = objectStream.readObject();
+
+                objects.add((Serializable) object);
+            }
         } catch (IOException | ClassNotFoundException e) {
             throw new RuntimeException("Could not read serialized file: " + e.getMessage());
         }
+
+        return objects;
     }
 }
