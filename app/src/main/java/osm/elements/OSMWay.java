a86c5a5ebaa771b9c9871db2652fe20842ea7189
JonasUJ <jonas@ulsnes.dk>
2022-03-11 17:46:06 +0100

    Introduce Slim versions of all OSMElements
    They're slim because I've stripped the tags off of them. Tags are costly to store because each tag list uses at least an ArrayList of memory. This way the tags are available to all observers when the element has been parsed, but will not be passed again later.

--- app/src/main/java/osm/elements/OSMWay.java
+++ app/src/main/java/osm/elements/OSMWay.java
@@ -4,31 +4,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public final class OSMWay extends OSMElement {
-    private final long id;
-    private final List<OSMNode> nodes;
+    private final SlimOSMWay slim;
     private final List<OSMTag> tags;
 
     public OSMWay(long id) {
         this(id, new ArrayList<>(), new ArrayList<>());
     }
 
-    public OSMWay(long id, List<OSMNode> nodes, List<OSMTag> tags) {
-        this.id = id;
-        this.nodes = nodes;
+    public OSMWay(long id, List<SlimOSMNode> nodes, List<OSMTag> tags) {
+        this.slim = new SlimOSMWay(id, nodes);
         this.tags = tags;
     }
 
     @Override
     public long id() {
-        return id;
+        return slim.id();
     }
 
-    public List<OSMNode> nodes() {
-        return nodes;
+    public List<SlimOSMNode> nodes() {
+        return slim.nodes();
     }
 
     @Override
     public List<OSMTag> tags() {
         return tags;
+    }
+
+    public SlimOSMWay slim() {
+        return slim;
     }
 }
