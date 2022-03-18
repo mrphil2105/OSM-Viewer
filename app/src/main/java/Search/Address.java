a74dab1be529fc3ca4b9aba3f0430214e17bc52c
magnusblarsen <magnusbrunlarsen00@gmail.com>
2022-03-18 12:34:10 +0100

    Implement parsing of searched addresses

--- app/src/main/java/Search/Address.java
+++ app/src/main/java/Search/Address.java
@@ -1,3 +1,8 @@
 package Search;
 
-public record Address(String street, String houseNumber, String city, int postcode) {}
+public record Address(String street, String houseNumber, String city, int postcode) {
+    @Override
+    public String toString(){
+        return street + " " + houseNumber + " " + city + " " + postcode;
+    }
+}
