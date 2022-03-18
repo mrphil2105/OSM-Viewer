f1682d348efead1a12bd81a12c9fffbde99c50e9
magnusblarsen <magnusbrunlarsen00@gmail.com>
2022-03-18 12:34:10 +0100

    Implement parsing of searched addresses

--- app/src/main/java/Search/AddressDatabase.java
+++ app/src/main/java/Search/AddressDatabase.java
@@ -1,27 +1,33 @@
 package Search;
 
 import collections.trie.Trie;
+import collections.trie.FinalTrie;
 import collections.trie.TrieBuilder;
+import collections.trie.TrieIterator;
 import osm.OSMObserver;
 import osm.elements.OSMNode;
 
-import java.util.Arrays;
-import java.util.Iterator;
+import java.util.*;
 import java.util.Map.Entry;
+import java.util.regex.Pattern;
 
 
 public class AddressDatabase implements OSMObserver {
-    Trie<Address> streetToAddress;
-    TrieBuilder<Address> trieBuilder;
-
+    private Trie<Address> streetToAddress;
+    private TrieBuilder<Address> trieBuilder;
+    private List<Address> history;
 
     public AddressDatabase() {
         trieBuilder = new TrieBuilder<>('\0');
+        history = getHistory();
+    }
 
+    public List<Address> getHistory(){
+        return history;
     }
 
     public void addAddress(Address a){
-        trieBuilder.put(a.street(), a);
+        trieBuilder.put(a.street() + " " + a.houseNumber(), a);
     }
 
     @Override
@@ -33,26 +39,84 @@ public class AddressDatabase implements OSMObserver {
         streetToAddress = trieBuilder.build();
     }
 
-
-
-    public Iterator<Entry<String,Address>> search(String search){
-        return streetToAddress.withPrefix("Sv");
+    public Iterator<Entry<String,Address>> streetSearch(String search){
+        return streetToAddress.withPrefix(search);
     }
+
+    private final static String REGEX = "^[ .,]*(?<street>[A-Za-zæøåÆØÅ.é ]+?)(([ .,]+)(?<house>[0-9]+[A-Za-z]?(-[0-9]+)?)([ ,.]+(?<floor>[0-9]{1,3})([ ,.]+(?<side>tv|th|mf|[0-9]{1,3})?))?([ ,.]*(?<postcode>[0-9]{4})??[ ,.]*(?<city>[A-Za-zæøåÆØÅ ]+?)?)?)?[ ,.]*$";
+    private final static Pattern PATTERN = Pattern.compile(REGEX);
+
+    public List<Address> searchAddress(String input) {
+        var matcher = PATTERN.matcher(input);
+        List<Address> results;
+
+        if (!matcher.matches()) {
+            return null;
+        }
+
+        var street = matcher.group("street");
+        var searchedStreets = streetSearch(street);
+        var parsedAddresses = new TreeSet<String>();
+        results = new LinkedList<>();
+
+        int entriesAdded = 0;
+        int maxEntries = 5; //TODO: it shouldnt be hardcoded
+        while(searchedStreets.hasNext() && entriesAdded < maxEntries){
+            entriesAdded++;
+            var entry = searchedStreets.next().getValue();
+
+            String city = entry.city() == null ? "" : entry.city();
+
+            if(parsedAddresses.add(entry.street() + city)){
+                results.add(entry);
+            }
+        }
+
+        //TODO: we also need to save these
+        matcher.group("house");
+        matcher.group("floor");
+        matcher.group("side");
+
+        return results;
+    }
+
+
+
 
     //Temporary test method
     public static void main(String[] args) {
         var db = new AddressDatabase();
 
-        Address[] addresses = new Address[2]; //TODO
+        var sv = new Address("Svend","3","SOlrød",2680);
+        var sve = new Address("Svend","4","SOlrød",2680);
+        var svend = new Address("Svend","2","SOlrød",2680);
+        var rued = new Address("Rued","2","SOlrød",2680);
+        Address[] addresses = new Address[]{sv, sve, svend, rued}; //TODO
         
         Arrays.stream(addresses).forEach(db::addAddress);
 
-        var results = db.search("Sv");
+        db.buildTries();
 
-        while(results.hasNext()){
-            var element = results.next();
+        Iterator<Entry<String, Address>> result = db.streetSearch("Sv");
+
+        System.out.println("FIRST");
+        while(result.hasNext()){
+            var element = result.next();
             System.out.println(element.getValue().street());
         }
+
+
+        /*
+        var result2 = result.withPrefixTest("e");
+        if(result2 != null){
+            System.out.println("SECOND");
+            var iterator2 = result2.iterator();
+            while(iterator2.hasNext()){
+                var element = iterator2.next();
+                System.out.println(element.getValue().street());
+            }
+        }
+         */
     }
 
 
