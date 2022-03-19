4f01f1551a1733a9e40b7a9503d86f43418bc697
Philip Mørch <mrphil2105@gmail.com>
2022-03-19 16:48:04 +0100

    Add comments explaining substantial parts of the code

--- app/src/main/java/collections/spacial/TwoDTree.java
+++ app/src/main/java/collections/spacial/TwoDTree.java
@@ -33,19 +33,26 @@ public class TwoDTree<E> extends SpacialTree<E> {
 
     private Node insert(E value, Point point, Node node, int level) {
         if (node == null) {
+            // The base case, insert a new node by returning it to the parent.
             size++;
 
             return new Node(value, point, null);
         }
 
         if (node.point.equals(point)) {
+            // Just return if we insert an existing point.
             return node;
         }
 
         if (level % 2 == 0) {
+            // Search by y-coordinate (point with horizontal partition line).
+
+            // We want to call insert on the left side if the new point is smaller at the y-axis.
             if (point.y() < node.y()) {
+                // Traverse down the tree. If this is a null child an insert will be performed.
                 node.left = insert(value, point, node.left, level + 1);
 
+                // If the child node has an uninitialized rect we initialize it.
                 if (node.left.rect == null) {
                     node.left.rect = new Rect(node.rect.xMin(), node.rect.yMin(), node.rect.xMax(), node.y());
                 }
@@ -58,6 +65,9 @@ public class TwoDTree<E> extends SpacialTree<E> {
                 }
             }
         } else {
+            // Search by x-coordinate (point with vertical partition line).
+
+            // We want to call insert on the left side if the new point is smaller at the x-axis.
             if (point.x() < node.x()) {
                 node.left = insert(value, point, node.left, level + 1);
 
@@ -92,12 +102,18 @@ public class TwoDTree<E> extends SpacialTree<E> {
         }
 
         if (level % 2 == 0) {
+            // Search by y-coordinate (point with horizontal partition line).
+
+            // We want to check the left side if the point is smaller at the y-axis.
             if (point.y() < node.y()) {
                 return contains(point, node.left, level + 1);
             } else {
                 return contains(point, node.right, level + 1);
             }
         } else {
+            // Search by x-coordinate (point with vertical partition line).
+
+            // We want to check the left side if the point is smaller at the x-axis.
             if (point.x() < node.x()) {
                 return contains(point, node.left, level + 1);
             } else {
@@ -123,22 +139,36 @@ public class TwoDTree<E> extends SpacialTree<E> {
     }
 
     private QueryResult nearest(Point query, Node node, QueryResult champ, float best, int level) {
+        // Check if the distance from the query point to the nearest point of
+        // the node rectangle is greater than the current best distance.
         if (node == null || best < node.rect.distanceSquaredTo(query)) {
             return champ;
         }
 
+        // Set an actual best distance when we recur.
         best = champ.point().distanceSquaredTo(query);
         var dist = node.point.distanceSquaredTo(query);
 
+        // Check if the distance from the query point to the traversed point is less than
+        // the distance from the query point to the current champion point.
         if (dist < best) {
             best = dist;
             champ = new QueryResult(node.value, node.point);
         }
 
         if (level % 2 == 0) {
+            // Search by y-coordinate (point with horizontal partition line).
+
+            // We want to check the right side if the query point is greater at the y-axis.
             if (node.y() < query.y()) {
+                // Traverse down the tree to check child nodes.
                 champ = nearest(query, node.right, champ, best, level + 1);
 
+                // Decide if we need to go down and check the other child.
+                // Compare the distance from the query point to the nearest point of the node rectangle
+                // against the distance from the query point to the current champion point.
+                // If it is smaller, there could potentially be a point that is closer to the query point
+                // than the current champion point.
                 if (node.left != null
                         && node.left.rect.distanceSquaredTo(query) < champ.point().distanceSquaredTo(query)) {
                     champ = nearest(query, node.left, champ, best, level + 1);
@@ -152,6 +182,9 @@ public class TwoDTree<E> extends SpacialTree<E> {
                 }
             }
         } else {
+            // Search by x-coordinate (point with vertical partition line).
+
+            // We want to check the right side if the query point is greater at the x-axis.
             if (node.x() < query.x()) {
                 champ = nearest(query, node.right, champ, best, level + 1);
 
