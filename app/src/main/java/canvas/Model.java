28732d532afbe77fbcaa6888b2abe29227ac5605
alberted115 <alberted115@gmail.com>
2022-03-23 14:22:29 +0100

    ger

--- app/src/main/java/canvas/Model.java
+++ app/src/main/java/canvas/Model.java
@@ -36,7 +36,22 @@ public class Model {
             addresses.buildTries();
         }
 
-        addresses.display();
+
+        System.out.println("1");
+        for (Address a : addresses.searchAddress("Ibskervej")){
+            System.out.println(a.toString());
+        }
+        System.out.println("2");
+        for (Address a : addresses.searchAddress("Ibskervej 41")){
+
+            System.out.println(a.toString());
+        }
+        System.out.println("3");
+        for (Address a : addresses.searchAddress("Ibskervej 2 Nex")){
+
+            System.out.println(a.toString());
+        }
+
     }
 
     private void loadPolygons(PolygonsReader reader) {
