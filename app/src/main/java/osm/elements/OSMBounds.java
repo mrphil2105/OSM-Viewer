a457bcd7457cd4c7af157751eced31a876ef2196
JonasUJ <jonas@ulsnes.dk>
2022-03-27 13:54:59 +0200

    Change Rect constructor paramter order

--- app/src/main/java/osm/elements/OSMBounds.java
+++ app/src/main/java/osm/elements/OSMBounds.java
@@ -1,3 +1,9 @@
 package osm.elements;
 
-public record OSMBounds(double minlat, double minlon, double maxlat, double maxlon) {}
+import geometry.Rect;
+
+public record OSMBounds(double minlat, double minlon, double maxlat, double maxlon) {
+    public Rect rect() {
+        return new Rect((float) minlat(), (float) minlon(), (float) maxlat(), (float) maxlon());
+    }
+}
