8acc8e4ae8e9118500ab774a9f1e8a2f261ecb65
alberted115 <alberted115@gmail.com>
2022-03-30 14:56:31 +0200

    Implemented finding address

--- app/src/main/java/Search/AutofillContextMenu.java
+++ app/src/main/java/Search/AutofillContextMenu.java
@@ -55,5 +55,7 @@ public class AutofillContextMenu extends ContextMenu {
         }else{
             text.end();
         }
+
+        ((AutofillTextField)text).setCurrentAddress(menuAddress);
     }
 }
