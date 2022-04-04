e518cab8dedde5880e4460c898901c0bb7627943
alberted <alberted115@gmail.com>
2022-04-04 18:48:34 +0200

    Fine tuning look of hbox

--- app/src/main/java/pointsOfInterest/PointsOfInterestHBox.java
+++ app/src/main/java/pointsOfInterest/PointsOfInterestHBox.java
@@ -3,6 +3,7 @@ package pointsOfInterest;
 import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
 import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
 public class PointsOfInterestHBox extends HBox {
@@ -13,10 +14,18 @@ public class PointsOfInterestHBox extends HBox {
     private Button remove;
 
     public PointsOfInterestHBox(PointOfInterest point){
+        setMaxWidth(140);
         text=new Text(point.name());
+        text.setFont(new Font(15));
         pointOfInterest=point;
         find=new Button("Find");
         remove=new Button("Remove");
+        var width=90;
+        var fontSize =10;
+        find.setPrefSize(width,10);
+        remove.setPrefSize(width,10);
+        find.setFont(new Font(fontSize));
+        remove.setFont(new Font(fontSize));
         getChildren().add(text);
         getChildren().add(find);
         getChildren().add(remove);
