d4d3381f5c842200696d37ac617c2bdf1e294e1b
alberted115 <alberted115@gmail.com>
2022-04-06 11:37:54 +0200

    Made remove buttons fit visual modes

--- app/src/main/java/application/App.java
+++ app/src/main/java/application/App.java
@@ -8,7 +8,7 @@ public class App extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        var model = new Model("data/bornholm.xml.zip");
+        var model = new Model("data/bornholm.gz.tar");
 
         new View(model, primaryStage);
     }
