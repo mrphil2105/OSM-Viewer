79fa0686b591208b006a12dc571be8896c627259
JonasUJ <jonas@ulsnes.dk>
2022-04-15 00:09:30 +0200

    Move non-canvas code out of MapCanvas
    Somehow the MapCanvas and controller became the main view controller, which was not intented or correct

--- app/src/main/java/view/Controller.java
+++ app/src/main/java/view/Controller.java
@@ -1,46 +1,46 @@
-package canvas;
+package view;
 
 import Search.SearchTextField;
-import com.jogamp.newt.event.MouseEvent;
-import com.jogamp.newt.event.MouseListener;
+import canvas.MapCanvas;
+import canvas.Renderer;
 import drawing.Category;
 import drawing.Drawable;
 import drawing.Drawing;
 import geometry.Point;
+import geometry.Vector2D;
 import java.util.Arrays;
 import java.util.Timer;
 import java.util.TimerTask;
-
 import javafx.application.Platform;
 import javafx.beans.binding.Bindings;
-import geometry.Vector2D;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.geometry.Point2D;
 import javafx.geometry.Side;
 import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.input.MouseButton;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 import pointsOfInterest.PointOfInterest;
 import pointsOfInterest.PointsOfInterestHBox;
 import pointsOfInterest.PointsOfInterestVBox;
 
-public class Controller implements MouseListener {
+public class Controller {
     private Model model;
-    private Point2D lastMouse;
-
     private Timer queryPointTimer;
-
     private boolean pointOfInterestMode = false;
     private Tooltip addPointOfInterestText;
-
     private Drawing lastDrawnAddress;
 
     @FXML private MapCanvas canvas;
+
+    @FXML private Button searchButton;
+
+    @FXML private Scene scene;
+
+    @FXML private Button addPointOfInterest;
+
+    @FXML private HBox pointsOfInterestHeader;
 
     @FXML private Menu categories;
 
@@ -85,9 +85,10 @@ public class Controller implements MouseListener {
     public void init(Model model) {
         this.model = model;
 
-        canvas.init(model);
-        canvas.addMouseListener(this);
+        canvas.init(model.canvasModel);
+
         searchTextField.init(model.getAddresses());
+
         checkBoxBuildings.setSelected(true);
         checkBoxHighways.setSelected(true);
         checkBoxWater.setSelected(true);
@@ -95,8 +96,77 @@ public class Controller implements MouseListener {
         radioButtonCar.setSelected(true);
         setStyleSheets("style.css");
 
-        nearestRoadLabel.textProperty().bind(Bindings.concat("Nearest road: ", model.nearestRoadProperty()));
+        nearestRoadLabel
+                .textProperty()
+                .bind(Bindings.concat("Nearest road: ", model.nearestRoadProperty()));
         pointsOfInterestVBox.init(model.getPointsOfInterest());
+
+        canvas.mapMouseClickedProperty.set(
+                e -> {
+                    if (pointOfInterestMode) {
+                        Point point = canvas.canvasToMap(new Point((float) e.getX(), (float) e.getY()));
+                        var cm = new ContextMenu();
+                        var tf = new TextField("POI name");
+                        var mi = new CustomMenuItem(tf);
+                        mi.setHideOnClick(false);
+                        cm.getItems().add(mi);
+
+                        cm.show(canvas, Side.LEFT, e.getX(), e.getY());
+                        tf.requestFocus();
+                        canvas.giveFocus();
+
+                        tf.setOnAction(
+                                a -> {
+                                    addPointOfInterest(
+                                            new PointOfInterest(
+                                                    point.x(),
+                                                    point.y(),
+                                                    tf.getText(),
+                                                    Drawing.create(new Vector2D(point), Drawable.POI)));
+                                    cm.hide();
+                                });
+
+                        pointOfInterestMode = false;
+                        addPointOfInterestText.hide();
+                    }
+                });
+
+        canvas.mapMouseMovedProperty.set(
+                e -> {
+                    Runnable queryRunnable =
+                            () -> {
+                                var mousePoint = new Point(e.getX(), e.getY());
+                                var queryPoint = canvas.canvasToMap(mousePoint);
+                                model.setQueryPoint(queryPoint);
+                            };
+
+                    if (nearestRoadDelayItem.isSelected()) {
+                        if (queryPointTimer != null) {
+                            queryPointTimer.cancel();
+                        }
+
+                        queryPointTimer = new Timer();
+                        queryPointTimer.schedule(
+                                new TimerTask() {
+                                    @Override
+                                    public void run() {
+                                        Platform.runLater(queryRunnable);
+                                    }
+                                },
+                                50);
+                    } else {
+                        queryRunnable.run();
+                    }
+
+                    if (pointOfInterestMode) {
+                        var bounds = canvas.getBoundsInLocal();
+                        var screenBounds = canvas.localToScreen(bounds);
+                        addPointOfInterestText.show(
+                                canvas,
+                                e.getX() + screenBounds.getMinX() + 50,
+                                e.getY() + screenBounds.getMinY() - 30);
+                    }
+                });
 
         // FIXME: yuck
         categories
@@ -143,20 +213,20 @@ public class Controller implements MouseListener {
     @FXML
     public void handleSearchClick() {
         var address = searchTextField.handleSearch();
-        if (address == null) return; //TODO: handle exception and show message?
-        Point point = Point.geoToMap(new Point((float)address.node().lon(),(float)address.node().lat()));
+        if (address == null) return; // TODO: handle exception and show message?
+        Point point =
+                Point.geoToMap(new Point((float) address.node().lon(), (float) address.node().lat()));
         zoomOn(point);
-        var drawing=Drawing.create(new Vector2D(point), Drawable.ADDRESS);
+        var drawing = Drawing.create(new Vector2D(point), Drawable.ADDRESS);
         canvas.getRenderer().draw(drawing);
-        if (lastDrawnAddress!=null){
+        if (lastDrawnAddress != null) {
             canvas.getRenderer().clear(lastDrawnAddress);
         }
-        lastDrawnAddress=drawing;
-
+        lastDrawnAddress = drawing;
     }
 
     @FXML
-    public void handleInFocus(){
+    public void handleInFocus() {
         searchTextField.showHistory();
     }
 
@@ -190,98 +260,6 @@ public class Controller implements MouseListener {
         }
     }
 
-    @Override
-    public void mouseClicked(MouseEvent mouseEvent) {
-      if (pointOfInterestMode){
-          Point point = canvas.canvasToMap(new Point((float)mouseEvent.getX(),(float)mouseEvent.getY()));
-          var cm = new ContextMenu();
-          var tf = new TextField("POI name");
-          var mi = new CustomMenuItem(tf);
-          mi.setHideOnClick(false);
-          cm.getItems().add(mi);
-
-          cm.show(canvas, Side.LEFT, mouseEvent.getX(), mouseEvent.getY());
-          tf.requestFocus();
-          canvas.giveFocus();
-
-          tf.setOnAction(e -> {
-              addPointOfInterest(new PointOfInterest(point.x(),point.y(),tf.getText(),Drawing.create(new Vector2D(point),Drawable.POI)));
-              cm.hide();
-          });
-        pointOfInterestMode=false;
-        addPointOfInterestText.hide();
-
-      }
-    }
-
-    @Override
-    public void mouseEntered(MouseEvent mouseEvent) {}
-
-    @Override
-    public void mouseExited(MouseEvent mouseEvent) {}
-
-    @Override
-    public void mousePressed(MouseEvent mouseEvent) {
-        lastMouse = new Point2D(mouseEvent.getX(), mouseEvent.getY());
-    }
-
-    @Override
-    public void mouseReleased(MouseEvent mouseEvent) {}
-
-    @Override
-    public void mouseMoved(MouseEvent mouseEvent) {
-        Runnable queryRunnable = () -> {
-            var mousePoint = new Point(mouseEvent.getX(), mouseEvent.getY());
-            var queryPoint = canvas.canvasToMap(mousePoint);
-            model.setQueryPoint(queryPoint);
-        };
-
-        if (nearestRoadDelayItem.isSelected()) {
-            if (queryPointTimer != null) {
-                queryPointTimer.cancel();
-            }
-
-            queryPointTimer = new Timer();
-            queryPointTimer.schedule(new TimerTask() {
-                @Override
-                public void run() {
-                    Platform.runLater(queryRunnable);
-                }
-            }, 50);
-        } else {
-            queryRunnable.run();
-        }
-
-        if (pointOfInterestMode){
-            var bounds =canvas.getBoundsInLocal();
-            var screenBounds=canvas.localToScreen(bounds);
-            addPointOfInterestText.show(canvas,mouseEvent.getX()+screenBounds.getMinX()+50, mouseEvent.getY()+screenBounds.getMinY()-30);
-        }
-    }
-
-    @Override
-    public void mouseDragged(MouseEvent mouseEvent) {
-        canvas.pan(
-                (float) (mouseEvent.getX() - lastMouse.getX()),
-                (float) (mouseEvent.getY() - lastMouse.getY()));
-        lastMouse = new Point2D(mouseEvent.getX(), mouseEvent.getY());
-    }
-
-    @Override
-    public void mouseWheelMoved(MouseEvent mouseEvent) {
-        if (mouseEvent.getRotation()[0] == 0.0) {
-            canvas.zoom(
-                    (float) Math.pow(1.05, mouseEvent.getRotation()[1]),
-                    mouseEvent.getX(),
-                    mouseEvent.getY());
-        } else {
-            canvas.zoom(
-                    (float) Math.pow(1.15, mouseEvent.getRotation()[0]),
-                    mouseEvent.getX(),
-                    mouseEvent.getY());
-        }
-    }
-
     public void setStyleSheets(String stylesheet) {
         leftVBox.getStylesheets().clear();
         leftVBox.getStylesheets().add(getClass().getResource(stylesheet).toExternalForm());
@@ -292,42 +270,48 @@ public class Controller implements MouseListener {
     public void center(Point point) {
         canvas.center(point);
     }
+
     public void zoomOn(Point point) {
-        canvas.zoomOn(point);
+        canvas.zoomTo(point);
     }
 
-    public void addPointOfInterest(PointOfInterest point){
+    public void addPointOfInterest(PointOfInterest point) {
         model.getPointsOfInterest().add(point);
         canvas.getRenderer().draw(point.drawing());
         pointsOfInterestVBox.update();
-        for (Node n:pointsOfInterestVBox.getChildren()){
+        for (Node n : pointsOfInterestVBox.getChildren()) {
 
-            if (((PointsOfInterestHBox)n).getPointOfInterest()==point){
-               var hBox = (PointsOfInterestHBox)n;
-               hBox.getFind().setOnAction(e -> {
-                  zoomOn(new Point(hBox.getPointOfInterest().lon(),hBox.getPointOfInterest().lat()));
-               });
-               hBox.getRemove().setOnAction(e -> {
-                    model.getPointsOfInterest().remove(hBox.getPointOfInterest());
-                    canvas.getRenderer().clear(hBox.getPointOfInterest().drawing());
-                    pointsOfInterestVBox.update();
-               });
+            if (((PointsOfInterestHBox) n).getPointOfInterest() == point) {
+                var hBox = (PointsOfInterestHBox) n;
+                hBox.getFind()
+                        .setOnAction(
+                                e -> {
+                                    zoomOn(
+                                            new Point(hBox.getPointOfInterest().lon(), hBox.getPointOfInterest().lat()));
+                                });
+                hBox.getRemove()
+                        .setOnAction(
+                                e -> {
+                                    model.getPointsOfInterest().remove(hBox.getPointOfInterest());
+                                    canvas.getRenderer().clear(hBox.getPointOfInterest().drawing());
+                                    pointsOfInterestVBox.update();
+                                });
             }
         }
     }
 
     @FXML
     public void enterPointOfInterestMode(ActionEvent actionEvent) {
-        if (addPointOfInterestText==null){
-            addPointOfInterestText=new Tooltip("Place point of interest on map");
+        if (addPointOfInterestText == null) {
+            addPointOfInterestText = new Tooltip("Place point of interest on map");
             addPointOfInterestText.requestFocus();
             canvas.giveFocus();
         }
 
-        var bounds =rightVBox.getBoundsInLocal();
-        var screenBounds=rightVBox.localToScreen(bounds);
-        addPointOfInterestText.show(rightVBox,screenBounds.getMinX(),screenBounds.getMinY()+230);
+        var bounds = rightVBox.getBoundsInLocal();
+        var screenBounds = rightVBox.localToScreen(bounds);
+        addPointOfInterestText.show(rightVBox, screenBounds.getMinX(), screenBounds.getMinY() + 230);
 
-        pointOfInterestMode=true;
+        pointOfInterestMode = true;
     }
 }
