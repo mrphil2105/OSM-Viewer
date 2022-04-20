9585b5352027808c12e5b8ec4b042e448f7ec05b
Amalie (amdh) <amdh@itu.dk>
2022-04-20 10:29:48 +0200

    CleanUp

--- app/src/main/java/canvas/Model.java
+++ app/src/main/java/canvas/Model.java
@@ -177,4 +177,28 @@ public class Model {
     public List<PointOfInterest> getPointsOfInterest() {
         return PointsOfInterest;
     }
+
+    private final float R = 6371; //Earth radius in km
+
+    public float getScaleBarDistance(){
+        float lon1 = bounds.getBottomLeft().x();
+        float lat1 = bounds.getBottomLeft().y();
+        float lon2 = bounds.getBottomRight().x();
+        float lat2 = bounds.getBottomRight().y();
+        //Haversine method
+        var deltaLat = degreeToRadian(lat2-lat1);
+        var deltaLon = degreeToRadian(lon2-lon1);
+        var a = 
+            Math.sin(deltaLat/2) * Math.sin(deltaLat/2) + 
+            Math.cos(degreeToRadian(lat1)) * Math.cos(degreeToRadian(lat2))*
+            Math.sin(deltaLon/2) * Math.sin(deltaLon/2)
+            ;
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        var d = R * c;
+        return (float) (d * 1000); //convert to metres
+    }
+
+    private float degreeToRadian(float degree){
+        return (float) (degree * (Math.PI/180));
+    }
 }
