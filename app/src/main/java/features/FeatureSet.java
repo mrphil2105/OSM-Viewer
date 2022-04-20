58c1c1cc18c4b850c9282aa5a3f2754e366d8378
JonasUJ <jonas@ulsnes.dk>
2022-04-20 12:39:13 +0200

    Add FeatureSet.ALL

--- app/src/main/java/features/FeatureSet.java
+++ app/src/main/java/features/FeatureSet.java
@@ -3,6 +3,16 @@ package features;
 import osm.OSMObserver;
 
 import java.io.Serializable;
+import java.util.EnumSet;
 import java.util.HashSet;
+import java.util.Set;
 
-public class FeatureSet extends HashSet<Feature> implements OSMObserver, Serializable {}
+public class FeatureSet extends HashSet<Feature> implements OSMObserver, Serializable {
+    public static final FeatureSet ALL = new FeatureSet(EnumSet.allOf(Feature.class));
+
+    public FeatureSet() {}
+
+    public FeatureSet(Set<Feature> set) {
+        addAll(set);
+    }
+}
