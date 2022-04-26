975d101c758e3b3e46a5159fcbdf3dd9620c61bf
JonasUJ <jonas@ulsnes.dk>
2022-04-26 22:33:35 +0200

    Add LoadingDialog and timer to CreateMapDialog

--- app/src/main/java/dialog/Dialog.java
+++ app/src/main/java/dialog/Dialog.java
@@ -1,6 +1,7 @@
 package dialog;
 
 import java.io.IOException;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -9,6 +10,7 @@ import javafx.stage.Stage;
 
 public abstract class Dialog {
     private Stage stage;
+    private boolean closable;
 
     protected static Dialog load(String resource) throws IOException {
         var fxmlLoader = new FXMLLoader(Dialog.class.getResource(resource));
@@ -20,6 +22,10 @@ public abstract class Dialog {
         diag.stage.initModality(Modality.APPLICATION_MODAL);
         diag.stage.setScene(new Scene(root));
         diag.stage.setResizable(false);
+        diag.stage.setOnCloseRequest(
+                e -> {
+                    if (!diag.closable) e.consume();
+                });
 
         return diag;
     }
@@ -28,7 +34,9 @@ public abstract class Dialog {
         stage.showAndWait();
     }
 
+    @FXML
     protected void close() {
+        closable = true;
         stage.close();
     }
 }
