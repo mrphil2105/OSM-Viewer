ed2c68f53a3da8fcb6eb720bd857b7c6a982ca2c
Amalie (amdh) <amdh@itu.dk>
2022-04-27 20:02:14 +0200

    more cleanUp

--- app/src/main/java/canvas/ZoomHandler.java
+++ app/src/main/java/canvas/ZoomHandler.java
@@ -11,11 +11,12 @@ public class ZoomHandler {
     private float max;
     private boolean isX;
 
-    public ZoomHandler(Rect bounds, MapCanvas canvas){
+    public ZoomHandler(Rect bounds, MapCanvas canvas) {
         this.bounds = bounds;
         this.canvas = canvas;
         currentScale = (float) (getScaleBarDistance() * (100/canvas.getPrefWidth()));
-        if (1280/(Point.geoToMap(bounds.getBottomRight()).x() - Point.geoToMap(bounds.getTopLeft()).x()) > 720/(Point.geoToMap(bounds.getTopLeft()).y() - Point.geoToMap(bounds.getBottomRight()).y())){
+        if (1280/(Point.geoToMap(bounds.getBottomRight()).x() - Point.geoToMap(bounds.getTopLeft()).x())
+                > 720/(Point.geoToMap(bounds.getTopLeft()).y() - Point.geoToMap(bounds.getBottomRight()).y())) {
             max = (Point.geoToMap(bounds.getTopLeft()).y() - Point.geoToMap(bounds.getBottomRight()).y());
             isX = false;
         }else {
@@ -24,46 +25,46 @@ public class ZoomHandler {
         } 
     }
     
-    public String getZoomString(){
+    public String getZoomString() {
         float zoom;
-        if (isX){
+        if (isX) {
             zoom = (20/max + 1 - ((canvas.canvasToMap(new Point(1280, 0)).x() - canvas.canvasToMap(new Point(0, 0)).x())/max))*100;
         }else {
             zoom = (20/max + 1 - ((canvas.canvasToMap(new Point(0, 720)).y() - canvas.canvasToMap(new Point(0, 0)).y())/max))*100;
         }
-        return Float.toString((float) (Math.round((zoom)*10.0)/10.0)) + "%";
+        return (float) (Math.round((zoom) * 10.0) / 10.0) + "%";
     }
 
-    public float getMaxZoom(){
-        if (isX){
+    public float getMaxZoom() {
+        if (isX) {
             return 1280/(max + 20);
         } else {
             return 720/(max + 20);
         }
     }
 
-    public float getMinZoom(){
-        if (isX){
-            return 1280/20;
+    public float getMinZoom() {
+        if (isX) {
+            return 64; //1280/20
         } else {
-            return 720/20;
+            return 36; //720/20
         }
     }
         
    public String getScaleString(){
         var newScale = (float) (currentScale *  (1/canvas.getZoom()));
-        if (newScale < 1000){
-            return Float.toString((float) (Math.round(newScale*100.0)/100.0)) + "m";
+        if (newScale < 1000) {
+            return (float) (Math.round(newScale * 100.0) / 100.0) + "m";
         } else {
-            return Float.toString((float) (Math.round((newScale/1000)*100.0)/100.0)) + "km";
+            return (float) (Math.round((newScale / 1000) * 100.0) / 100.0) + "km";
         } 
    }
 
-   public float getStartZoom(){
-        return (float) ((1280/(Point.geoToMap(bounds.getBottomRight()).x() - Point.geoToMap(bounds.getTopLeft()).x())));
+   public float getStartZoom() {
+        return (1280/(Point.geoToMap(bounds.getBottomRight()).x() - Point.geoToMap(bounds.getTopLeft()).x()));
     } 
 
-    private float getScaleBarDistance(){
+    private float getScaleBarDistance() {
         float lon1 = bounds.getBottomLeft().x();
         float lat1 = bounds.getBottomLeft().y();
         float lon2 = bounds.getBottomRight().x();
@@ -81,7 +82,7 @@ public class ZoomHandler {
         return (float) (d * 1000); //convert to metres
     }
 
-    private float degreeToRadian(float degree){
+    private float degreeToRadian(float degree) {
         return (float) (degree * (Math.PI/180));
     }
 }
