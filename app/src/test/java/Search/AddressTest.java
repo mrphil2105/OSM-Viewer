6703c9b43e989ef0b834b1ace4ed00df7d675806
alberted <alberted115@gmail.com>
2022-04-29 12:29:35 +0200

    Finished unit test

--- app/src/test/java/Search/AddressTest.java
+++ app/src/test/java/Search/AddressTest.java
@@ -8,6 +8,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AddressTest {
@@ -19,18 +20,19 @@ public class AddressTest {
    @BeforeAll
    static void initiateAddresses(){
       addressDatabase=new AddressDatabase();
-      addresses=new Address[10];
+      addresses=new Address[11];
 
-      addresses[0]=(new Address("Testvej","24",null,null,"4050","Testby",null));
-      addresses[1]=(new Address("Testvej","20",null,null,"4050","Testby",null));
+      addresses[0]=(new Address("Testvej","24B",null,null,"4050","Testby",null));
+      addresses[1]=(new Address("Testvej","6",null,null,"4050","Testby",null));
       addresses[2]=(new Address("Testvej","21",null,null,"4050","Testby",null));
       addresses[3]=(new Address("Testvej","5",null,null,"4080","Anden Testby",null));
-      addresses[4]=(new Address("Testvej","6",null,null,"4090","Tredje Testby",null));
+      addresses[4]=(new Address("Testvej","27",null,null,"4090","Tredje Testby",null));
       addresses[5]=(new Address("Anden Testvej","245",null,null,"4050","Testby",null));
-      addresses[6]=(new Address("Anden Testvej","3",null,null,"4050","Testby",null));
-      addresses[7]=(new Address("Anvej","45",null,null,"4080","Anden Testby",null));
+      addresses[6]=(new Address("Anden Testvej","3A",null,null,"4050","Testby",null));
+      addresses[7]=(new Address("Anvej","35",null,null,"4080","Anden Testby",null));
       addresses[8]=(new Address("Endu En Vej","21",null,null,"5080","Fjerde Testby",null));
       addresses[9]=(new Address("Også En Vej","26",null,null,"5080","Fjerde Testby",null));
+      addresses[10]=(new Address("Også En Vej","24",null,null,"6000","Fjerde Testby b",null));
 
       for (Address a:addresses){
           addressDatabase.addAddress(a);
@@ -40,82 +42,141 @@ public class AddressTest {
    }
 
 
+   //tests
 
     @Test
     public void testOnlyLetters(){
+        //should return addreses which street name starts with input letters,but only one per city
+
         var list = search("t");
+        int[] shouldContain = {0,3,4};
+        assertTrue(containsExactly(list,shouldContain));
 
-        var shouldContain=new ArrayList<Address>();
-        shouldContain.add(addresses[0]);
-        shouldContain.add(addresses[1]);
-        shouldContain.add(addresses[2]);
-        shouldContain.add(addresses[3]);
-        shouldContain.add(addresses[4]);
-
-        assertTrue(true);
+        var list2 = search(" An");
+        int[] shouldContain2 = {5,7};
+        assertTrue(containsExactly(list2,shouldContain2));
 
     }
 
-
    @Test
-   static void testOnlyStreets(){
-       var address = AddressDatabase.parse("");
-       address.street(SearchTextField.reformat(address.getStreet()));
-       address.city(SearchTextField.reformat(address.getCity()));
-        var searchList = addressDatabase.possibleAddresses(address.build(),5);
+   public void testOnlyStreets(){
+       //should return addresses with the given street name,but only one per city
+
+       var list = search("testvej");
+       int[] shouldContain = {0,3,4};
+       assertTrue(containsExactly(list,shouldContain));
+
+       var list2 = search("Endu En Vej");
+       int[] shouldContain2 = {8};
+       assertTrue(containsExactly(list2,shouldContain2));
    }
 
     @Test
-    static void testStreetAndHousenumber(){
-        var address = AddressDatabase.parse("");
-        address.street(SearchTextField.reformat(address.getStreet()));
-        address.city(SearchTextField.reformat(address.getCity()));
-        var searchList = addressDatabase.possibleAddresses(address.build(),5);
+    public void testStreetAndHousenumber(){
+        //should return addreses which street name starts with input letters, and which housenumber starts with the given number
+
+        var list = search("test 2");
+        int[] shouldContain = {0,2,4};
+        assertTrue(containsExactly(list,shouldContain));
+
+        var list2 = search(" Anden TestVej 3A,");
+        int[] shouldContain2 = {6};
+        assertTrue(containsExactly(list2,shouldContain2));
+
+        var list3 = search("An 3.");
+        int[] shouldContain3 = {6,7};
+        assertTrue(containsExactly(list3,shouldContain3));
 
     }
 
     @Test
-    static void testStreetAndHousenumberAndCity(){
-        var address = AddressDatabase.parse("");
-        address.street(SearchTextField.reformat(address.getStreet()));
-        address.city(SearchTextField.reformat(address.getCity()));
-        var searchList = addressDatabase.possibleAddresses(address.build(),5);
+    public void testStreetAndHousenumberAndCity(){
+        //should return addreses which street name starts with the first input letters, and which housenumber starts with the given number, and which city starts with the letters after the number
+
+        var list = search("testvej, 2 test");
+        int[] shouldContain = {0,2};
+        assertTrue(containsExactly(list,shouldContain));
+
+        var list2 = search("An 3, anden testby   ");
+        int[] shouldContain2 = {7};
+        assertTrue(containsExactly(list2,shouldContain2));
 
     }
 
     @Test
-    static void testFullAddress(){
-        var address = AddressDatabase.parse("");
-        address.street(SearchTextField.reformat(address.getStreet()));
-        address.city(SearchTextField.reformat(address.getCity()));
-        var searchList = addressDatabase.possibleAddresses(address.build(),5);
+    public void testStreetAndHousenumberAndPostcode(){
+        //should return addresses which street name starts with the first input letters, which housenumber starts with the given number,  and which postcode matches the second number string
+
+        var list = search("tesT 2 . 4090");
+        int[] shouldContain = {4};
+        assertTrue(containsExactly(list,shouldContain));
+
+        var list2 = search("An,, 3 4050");
+        int[] shouldContain2 = {6};
+        assertTrue(containsExactly(list2,shouldContain2));
+
+        var list3 = search("tesT 2 . 4050");
+        int[] shouldContain3 = {0,2};
+        assertTrue(containsExactly(list3,shouldContain3));
 
     }
 
     @Test
-    static void testFalseInput(){
-        var address = AddressDatabase.parse("");
-        address.street(SearchTextField.reformat(address.getStreet()));
-        address.city(SearchTextField.reformat(address.getCity()));
-        var searchList = addressDatabase.possibleAddresses(address.build(),5);
+    public void testFullAddress(){
+        //should return addresses which street name starts with the first input letters, which housenumber starts with the given number, and which city starts with the letters after the number, and which postcode matches the second number string
 
-        var address2 = AddressDatabase.parse("efAQEgsgrf");
-        address2.street(SearchTextField.reformat(address2.getStreet()));
-        address2.city(SearchTextField.reformat(address2.getCity()));
-        var searchList2 = addressDatabase.possibleAddresses(address2.build(),5);
+        var list = search("test  24B   4050 testBy ");
+        int[] shouldContain = {0};
+        assertTrue(containsExactly(list,shouldContain));
 
-        var address3 = AddressDatabase.parse("efAQEgsgrf");
-        address3.street(SearchTextField.reformat(address3.getStreet()));
-        address3.city(SearchTextField.reformat(address3.getCity()));
-        var searchList3 = addressDatabase.possibleAddresses(address3.build(),5);
+
+
+        var list2 = search("Også En Vej, 2 6000 fjerde ");
+        int[] shouldContain2 = {10};
+        assertTrue(containsExactly(list2,shouldContain2));
 
     }
+
+    @Test
+    public void testFalseInput(){
+        var list = search("");
+        assertTrue(list ==null || list.isEmpty());
+
+        var list2 = search("drgdfgdcfg");
+        assertTrue(list2 ==null || list2.isEmpty());
+
+        var list3 = search("An 3 anden testby 5060");
+        assertTrue(list3 ==null || list3.isEmpty());
+
+
+    }
+
+
+    //Utils
 
     private List<Address> search(String input){
         var address = AddressDatabase.parse(input);
+        if (address == null) {
+            return null;
+        }
+
         address.street(reformat(address.getStreet()));
         address.city(reformat(address.getCity()));
+
+
+
         return addressDatabase.possibleAddresses(address.build(),5);
+    }
+
+
+    private boolean containsExactly(List<Address> checkList, int[] numbers){
+        var shouldContain=new ArrayList<Address>();
+       for (int i : numbers){
+           shouldContain.add(addresses[i]);
+       }
+
+       return containsExactly(checkList,shouldContain);
+
     }
 
     private boolean containsExactly(List<Address> checkList,List<Address> input){
@@ -130,7 +191,6 @@ public class AddressTest {
 
     public static String reformat(String string) {
         if (string == null) return null;
-        System.out.println("yo");
         string = string.toLowerCase();
 
         var stringBuilder = new StringBuilder();
