772af9f10312dd9eb34f03545e56946683a83531
JonasUJ <jonas@ulsnes.dk>
2022-05-13 09:10:19 +0200

    Add more grid tests

--- app/src/test/java/collections/grid/GridTest.java
+++ app/src/test/java/collections/grid/GridTest.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import geometry.Point;
 import geometry.Rect;
+
+import java.util.HashMap;
 import java.util.NoSuchElementException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -86,5 +88,39 @@ public class GridTest {
         assertNotEquals(range1, range3);
         assertEquals(range2, range3);
         assertNotEquals(range3, new Point(0, 0));
+    }
+
+    @Test
+    public void testMapCtor() {
+        var map = new HashMap<Point, Integer>();
+        map.put(new Point(0, 0), 1);
+        map.put(new Point(1, 0), 2);
+        map.put(new Point(0, 1), 3);
+        map.put(new Point(1, 1), 4);
+
+        var grid = new Grid<Integer>(new Rect(0, 0, 10, 10), 5, map);
+
+        assertEquals(1, grid.get(new Point(3, 3)));
+        assertEquals(2, grid.get(new Point(6, 3)));
+        assertEquals(3, grid.get(new Point(3, 6)));
+        assertEquals(4, grid.get(new Point(6, 6)));
+    }
+
+    @Test
+    public void testSize() {
+        assertEquals(8, grid.size());
+    }
+
+    @Test
+    public void testIterator() {
+        var iter = grid.iterator();
+
+        for (int x = 0; x < 4; x++) {
+            for (int y = 0; y < 2; y++) {
+                assertEquals(new Point(x, y), iter.next());
+            }
+        }
+
+        assertThrows(NoSuchElementException.class, iter::next);
     }
 }
